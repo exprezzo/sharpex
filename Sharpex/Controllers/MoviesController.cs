bb006a45d8ca791122409615e68a6a4aaf9bcb56
Zesar Bibriesca <runtim3.error@gmail.com>
2012-08-27 16:03:40 -0600

    intentando usar los modelos

---
+++ Sharpex/Controllers/MoviesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sharpex.Models;
+
+namespace Sharpex.Controllers
+{
+    public class MoviesController : Controller
+    {
+        //
+        // GET: /Movies/
+        MoviesEntities db = new MoviesEntities();
+
+        public ActionResult Index()
+        {
+            var movies = from m in db.Movies
+                         where m.ReleaseDate > new DateTime(1984, 6, 1)
+                         select m;
+
+            return View(movies.ToList());
+
+        }
+
+    }
+}
